3d8021d5c8867a36342d6473c9e05d1617de4d26
z.Eftekhari <kavehahangar0ez@gmail.com>
2021-08-21 13:03:15 +0430

    Find genres

--- t1/play.js
+++ t1/play.js
@@ -1,1 +1,15 @@
 import { movies } from './data.js'
+
+//const myMovies = movies.filter(movie => movie.genres == "movie title")
+
+var unique_genres=[];
+
+movies.forEach(function(movie){
+   movie.genres.forEach(function(genre){
+        if(unique_genres.includes(genre)===false){
+            unique_genres.push(genre);
+        }
+   });
+});
+
+console.log(unique_genres);
